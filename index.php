91bb80ed62da869cfe42ef42d89723100658ee0f
Alex <slicer256@gmail.com>
2018-06-28 23:07:30 +0300

    simple bot with keyboard

--- index.php
+++ index.php
@@ -11,11 +11,23 @@ require_once 'vendor/autoload.php';
 require_once 'config.php';
 
 $bot = new \TelegramBot\Api\Client($config['bot_token']);
+$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([['/say Дай совет', '/start Поздороваться']], false);
 
+$bot->command('start', function ($message) use ($bot, $keyboard) {
+    $text = 'Привет, я Крипто-Костя. Я даю советы.';
+    $bot->sendMessage($message->getChat()->getId(), $text, null, false, null, $keyboard);
+});
 
-$bot->command('start', function ($message) use ($bot) {
-    $answer = 'Hello there :)';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
+$bot->command('say', function ($message) use ($bot, $keyboard) {
+    $tips = [
+        "Покупайте крипту по выгодному курсу.",
+        "Делайте иксы.",
+        "Спрашивайте у меня советы.",
+        "Крипта это хорошо.",
+        "Дайте подумать...",
+    ];
+    shuffle($tips);
+    $bot->sendMessage($message->getChat()->getId(), "Мой совет: " . $tips[0], null, false, null, $keyboard);
 });
 
 $bot->run();
